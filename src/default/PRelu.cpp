8d0142829ac02d0f8932c6a3e378cbd7366c236f
katsuhisa yuasa <berupon@gmail.com>
2021-10-21 23:05:12 +0900

    wip

--- src/default/PRelu.cpp
+++ src/default/PRelu.cpp
@@ -20,12 +20,12 @@ struct PRelu_operator : public operator_t {
 		const T* pa = (const T*)a->data;;
 
 		for (size_t i = 0, l = y->ndata; i < l; ++i) {
-			if (pa[i] < 0) {
+			T va = pa[i];
+			if (va < 0) {
 				const T* pb = (const T*)b->broadcast_map_address(y, i);
-				py[i] = pa[i] * (*pb);
+				va *= (*pb);
 			}
-			else
-				py[i] = pa[i];
+			py[i] = va;
 		}
 	}
 
