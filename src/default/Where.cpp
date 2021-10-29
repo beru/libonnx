02d2759952406ff98a7a024e2fbef0c03ae9bf12
katsuhisa yuasa <berupon@gmail.com>
2021-10-30 01:27:57 +0900

    wip

--- src/default/Where.cpp
+++ src/default/Where.cpp
@@ -12,12 +12,22 @@ struct Where_operator : public operator_t {
 	}
 
 	bool reshape() override {
-		tensor_t* y = outputs[0];
-		if (!y->reshape_identity(inputs[inputs.size() - 1])) {
+		const tensor_t* condition = inputs[0];
+		const tensor_t* x = inputs[1];
+		const tensor_t* y = inputs[2];
+		tensor_t* output = outputs[0];
+		if (condition->type != ONNX_TENSOR_TYPE_BOOL) {
+			return false;
+		}
+		if (x->type != y->type || x->type != output->type) {
+			return false;
+		}
+
+		if (!output->reshape_identity(inputs[inputs.size() - 1])) {
 			return false;
 		}
 		for (int i = inputs.size() - 2; i >= 0; i--) {
-			if (!y->reshape_multi_broadcast(y, inputs[i], y->type))
+			if (!output->reshape_multi_broadcast(output, inputs[i], output->type))
 				return false;
 		}
 		return true;
@@ -25,38 +35,48 @@ struct Where_operator : public operator_t {
 
 	template <typename T>
 	bool exec() {
-		tensor_t* y = outputs[0];
-		const tensor_t* x0 = inputs[0];
-		const tensor_t* x1 = inputs[1];
-		const tensor_t* x2 = inputs[2];
-		T* py = (T*)y->data;
-		T* px;
+		const tensor_t* condition = inputs[0];
+		const tensor_t* x = inputs[1];
+		const tensor_t* y = inputs[2];
+		tensor_t* output = outputs[0];
+		T* pout = (T*)output->data;
+		T* pin;
 
-		for (size_t i = 0, l = y->ndata; i < l; ++i) {
-			bool_t* c = (bool_t*)x0->broadcast_map_address(y, i);
+		for (size_t i = 0, l = output->ndata; i < l; ++i) {
+			bool_t* c = (bool_t*)condition->broadcast_map_address(output, i);
 			if (*c) {
-				px = (T*)x1->broadcast_map_address(y, i);
+				pin = (T*)x->broadcast_map_address(output, i);
 			}else {
-				px = (T*)x2->broadcast_map_address(y, i);
+				pin = (T*)y->broadcast_map_address(output, i);
 			}
-			py[i] = *px;
+			pout[i] = *pin;
 		}
 		return true;
 	}
 
 	bool exec() override {
-		tensor_type_t type = inputs[0]->type;
-		if (opset >= 9) {
-			if (inputs.size() == 3) {
-				return typed_exec<Where_operator,
-					bool_t,
-					uint8_t, uint16_t, uint32_t, uint64_t,
-					int8_t, int16_t, int32_t, int64_t,
-					float16_t, float, double, bfloat16_t,
-					std::complex<float>, std::complex<double>,
-					std::string
-				>(this, type);
-			}
+		tensor_type_t type = inputs[1]->type;
+		if (inputs.size() != 3) {
+			return false;
+		}
+		if (opset >= 16) {
+			return typed_exec<Where_operator,
+				bool_t,
+				uint8_t, uint16_t, uint32_t, uint64_t,
+				int8_t, int16_t, int32_t, int64_t,
+				float16_t, float, double, bfloat16_t,
+				std::complex<float>, std::complex<double>,
+				std::string
+			>(this, type);
+		}else if (opset >= 9) {
+			return typed_exec<Where_operator,
+				bool_t,
+				uint8_t, uint16_t, uint32_t, uint64_t,
+				int8_t, int16_t, int32_t, int64_t,
+				float16_t, float, double,
+				std::complex<float>, std::complex<double>,
+				std::string
+			>(this, type);
 		}
 		return false;
 	}
